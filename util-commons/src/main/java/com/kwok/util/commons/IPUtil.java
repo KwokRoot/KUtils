9662e90a3b85eb2128a90fd0313cc0731c59cd39
KwokRoot <KwokRoot@users.noreply.github.com>
2019-08-19 23:16:04 +0800

    Update IPUtil.java

--- util-commons/src/main/java/com/kwok/util/commons/IPUtil.java
+++ util-commons/src/main/java/com/kwok/util/commons/IPUtil.java
@@ -1,6 +1,9 @@
 package com.kwok.util.commons;
 
+import java.math.BigInteger;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -40,11 +43,61 @@ public class IPUtil {
 				+ ipNumArray[3]));
 	}
 
+	
+	/*
+	 * 10进制长整型数值 转为 IP 地址。
+	 */
+	public static String tenRadixLongConvertIP(final Long srcVal) throws Exception{
+    	
+		ArrayList<String> xRadixValArray = new ArrayList<String>();
+		
+		xRadixValArray = tenRadixLongConvertXRadixArray(srcVal, 256);
+    	
+		while (xRadixValArray.size() < 4){
+    		xRadixValArray.add(0, 0 + "");
+    	}
+    	
+		return xRadixValArray.stream().collect(Collectors.joining("."));
+    }
+    
+	
+	/*
+	 * 10 进制长整型数值 转为 X 进制数组
+	 */
+    private static ArrayList<String> tenRadixLongConvertXRadixArray(final Long srcVal, final long radix) throws Exception{
+    	
+    	if(radix <= 1){
+    		throw new Exception("参数 radix 有误，radix > 1");
+    	}
+    	
+    	ArrayList<String> xRadixValList = new ArrayList<String>();
+    	long temp = srcVal;
+    	xRadixValList.add(String.valueOf(temp % radix));
+    	temp = temp / radix;
+    	
+    	while (temp > 0) {
+    		xRadixValList.add(0, String.valueOf(temp % radix));
+    		temp = temp / radix;
+		}
+    	
+		return xRadixValList;
+    }
+	
 	public static void main(String[] args) throws Exception {
 
-		String ipStr = "61.087.255.255";
+		String ipStr = "61.87.255.255";
+		
 		try{
+			
 			System.out.println(IPFrom256RadixConvert10RadixLong(ipStr));
+			
+			System.out.println(tenRadixLongConvertIP(IPFrom256RadixConvert10RadixLong(ipStr)));
+			
+			/*
+			 * 测试 10 进制长整型数值 转为 X 进制数组
+			 */
+			System.out.println(new BigInteger("987654321").toString(2));
+			System.out.println(tenRadixLongConvertXRadixArray(Long.valueOf("987654321"), 2).stream().collect(Collectors.joining()));
 		}catch (Exception e) {
 			System.err.println(e.getMessage());
 		}
